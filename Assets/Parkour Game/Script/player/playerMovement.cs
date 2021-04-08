7e85e1398911596b66ad80507787ae8e9d40cd0b
Dhirajlal Vishwakarma <47329713+dhiraj7894@users.noreply.github.com>
2021-04-08 17:25:50 +0530

    update

---
+++ Assets/Parkour Game/Script/player/playerMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class playerMovement : MonoBehaviour
+{
+    public float _speed = 10;
+    public float _jumpHeight = 3f;
+    public Transform groundCheck;
+    public LayerMask ground;
+
+    Rigidbody rb;
+    private bool isGrounded = false;
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, 0.5f, ground);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.AddForce(Vector3.up * _jumpHeight * Time.deltaTime, ForceMode.Impulse);
+        }
+
+        raycastChecker(groundCheck.position, groundCheck.forward, 1.5f);
+    }
+
+    void raycastChecker(Vector3 targetPosition, Vector3 direction, float length)
+    {
+        Ray ray = new Ray(targetPosition, direction);
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, length))
+        {
+            rb.AddForce(Vector3.up * 30 * Time.deltaTime, ForceMode.Impulse);
+        }
+        else if(!Physics.Raycast(ray, out hit, length))
+        {
+            transform.Translate(transform.forward * _speed * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Parkour Game/Script/player/playerShootingSystem.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerShootingSystem : MonoBehaviour
+{
+    public Transform shootPoint;
+    public GameObject bullet;
+    public LineRenderer line;
+    public float _force = 50;
+    public float leserMaxLength = 10;
+
+    private GameObject bulletPrefeb;
+
+    void Update()
+    {
+        raycastChecker(shootPoint.position, shootPoint.forward, leserMaxLength);
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            bulletPrefeb = Instantiate(bullet, shootPoint.position, Quaternion.identity);
+            bulletPrefeb.GetComponent<Rigidbody>().AddForce(shootPoint.forward * _force, ForceMode.Impulse);
+            Destroy(bulletPrefeb, 3);
+        }
+    }
+
+    void raycastChecker(Vector3 targetPosition,Vector3 direction,float length)
+    {
+
+        Ray ray = new Ray(targetPosition, direction);
+        RaycastHit hit;
+        Vector3 endPos = targetPosition + (length * direction);
+
+        if(Physics.Raycast(ray,out hit, length))
+        {
+            endPos = hit.point;
+            line.enabled = true;
+            line.SetPosition(0, targetPosition);
+            line.SetPosition(1, endPos);
+        }
+        else
+        {
+            line.enabled = false;
+        }
+        
+    }
+}
